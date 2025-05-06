e35b01a1686dad08600b6fa7bcab8da83caebaa2
Braxton Brown <braxton.brown@outlook.com>
2025-05-06 00:30:59 -0600

    impl more delete and drop_table

--- src/traits.rs
+++ src/traits.rs
@@ -10,10 +10,13 @@ where
     fn column_getters() -> String {
         Self::column_names().join(",")
     }
+    /// Create the table in the database.
     fn create_table(conn: &rusqlite::Connection) -> Result<usize> {
         let sql = Self::create_table_str();
         conn.execute(&sql, ())
     }
+
+    /// Selects all rows from the table for which the where clause is true.
     fn select(
         conn: &rusqlite::Connection,
         where_clause: &str,
@@ -48,6 +51,21 @@ where
             .query_row(params, |row| Ok(Self::try_from(row)?))
             .optional()?;
         Ok(row)
+    }
+
+    fn delete(
+        conn: &rusqlite::Connection,
+        where_clause: &str,
+        params: impl rusqlite::Params,
+    ) -> rusqlite::Result<usize> {
+        let sql = format!("DELETE FROM {} {}", Self::TABLE_NAME, where_clause);
+        let mut stmt = conn.prepare(&sql).unwrap();
+        stmt.execute(params)
+    }
+
+    fn drop_table(conn: &rusqlite::Connection) -> rusqlite::Result<usize> {
+        let sql = format!("DROP TABLE IF EXISTS {}", Self::TABLE_NAME);
+        conn.execute(&sql, ())
     }
 }
 
